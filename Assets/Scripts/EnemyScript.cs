669e7c00a8832799c38f14f32ea37ce6e0bfe02b
l1992130 <l1992130@163.com>
2014-01-06 17:10:12 +0800

    第一次上传

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+
+	private WeaponScript[] weapons;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Awake()
+	{
+		weapons = GetComponentsInChildren<WeaponScript>();
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		foreach(WeaponScript weapon in weapons)
+			if(weapon != null && weapon.CanAttack)
+				weapon.Attack(true);
+	}
+}
---
+++ Assets/Scripts/HealthScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthScript : MonoBehaviour {
+
+	public int hp = 2;
+	public bool isEnemy = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		ShotScript shot = collider.gameObject.GetComponent<ShotScript>();
+		if(shot != null)
+		{
+			if(shot.isEnemyShot != isEnemy)
+			{
+				hp -= shot.damage;
+				Destroy(shot.gameObject);
+
+				if(hp <= 0)
+					Destroy(gameObject);
+			}
+		}
+	}
+}
